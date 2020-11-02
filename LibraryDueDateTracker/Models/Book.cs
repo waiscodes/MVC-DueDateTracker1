a25ef19a61b4a561e79a08d53951c76d6b0ded49
waiscodes <b.wais@outlook.com>
2020-11-02 13:49:10 -0700

    added book class, properties and constructor

---
+++ LibraryDueDateTracker/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LibraryDueDateTracker.Models
+{
+    public class Book
+    {
+        public int ID { get; }
+        public string Title { get; }
+        public string Author { get;  }
+        public DateTime PublicationDate { get;  }
+        public DateTime CheckOutDate { get;  }
+
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+        public Book(int id, string title, string author, DateTime publicationDate, DateTime checkoutDate)
+        {
+            ID = id;
+            Title = title;
+            Author = author;
+            PublicationDate = publicationDate;
+            CheckOutDate = checkoutDate;
+
+            //DueDate = DateTime + 14;
+            ReturnDate = null;
+        }
+    }
+}
